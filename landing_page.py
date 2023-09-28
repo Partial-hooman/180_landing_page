5001a4f3033270efc709f22e637c779b77ab3880
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-09-28 18:21:01 +0530

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-st.markdown(f'''[Click Here](https://stackoverflow.com)
-let's goooooo it works !''',
-unsafe_allow_html=True)
+st.write(f'''[Click Here](https://stackoverflow.com)
+let's goooooo it works !''')
