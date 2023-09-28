3a59b52b45da8268415bda57193ad9ab3d9a797e
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-09-28 18:30:34 +0530

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -1,9 +1,59 @@
 import streamlit as st
 
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
+multi = '''
+Link Checker - Checks a batch of domains to see if they pass 180’s quality guidelines.
 
-Two (or more) newline characters in a row will result in a hard return.
+
+Web Explorer Query Generator - Paste in competitor domains to create custom prospecting queries for Ahrefs Web Explorer.
+
+
+Prospecting Keyword Analyzer - Find prospecting keywords for your client by uploading competitor backlinks from Ahrefs Web Explorer.
+
+
+Prospecting Cleanup Tool - 
+Upload a CSV file from Ahrefs Content Explorer or Ahrefs Web Explorer and the tool removes duplicate domains, dead websites, and prospects on spammy TLDs.
+
+
+Content Explorer Personalizer (Content Refresh) - Provides personalized sentences you can use in your Content Refresh outreach emails.
+
+
+Subheading Suggestor (Content Refresh) - Suggests additional subheadings that can be added to your Content Refresh outreach emails.
+
+
+Pitchbox Tag Suggestor - Get suggestions on the best tags to use for any domain.
+
+
+Backlink Relevance Checker - Checks how relevant a batch of backlinks are to the client’s website.
+
+
+
+Keyword Cannibalization tool - Analyzes the client’s Queries report in Google Search Console to identify keyword cannibalization issues where multiple URLs are ranking for the same keywords. 
+
+
+Algorithmic Optimization Tool - Uses AI to automatically generate title tags and meta description for the top pages on the client’s website.
+
+
+Title Tag Checker - Identifies the best keyword for the top pages on the client’s website, and checks to see if the keyword exists in the page’s title tag.
+
+
+Internal Linking Tool - Paste in the blog post content as well as the website’s ranking data to quickly find internal linking opportunities.
+
+
+Bulk URL Removal Tool - Paste in a batch of URLs to be removed from Google’s index using the IndexNow API.
+
+
+Bulk URL Submission Tool - Paste in a batch of URLs to be removed from Google’s index using the IndexNow API.
+
+
+Google Autocomplete Scraper - Scrapes Google Autocomplete for relevant questions for up to 3 keywords. Very helpful for identifying subheadings for a blog post, or even blog post topics in general.
+
+
+Question Clustering Tool - Clusters 100’s or even 1,000’s of questions into different groups by analyzing the words within each keyword phrase. Great for identifying the main questions on a topic, especially while building a Topical Map.
+
+
+Relevant Page Finder - Input the client’s domain and up to 100 keywords to find the most relevant URL on the client’s website for each keyword.
+
+
 '''
 st.markdown(multi)
 
